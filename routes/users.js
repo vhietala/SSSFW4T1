6f025ec5112964c0547afb555affb45632e2fcec
vhietala <vhietala@gmail.com>
2018-04-10 20:28:57 +0300

    rest methods for user

--- routes/users.js
+++ routes/users.js
@@ -2,13 +2,24 @@ const express = require('express');
 const router = express.Router();
 
 /* GET users listing. */
-router.get('/:param',(req, res, next)=> {
-  res.send('get user with id: '+req.params.param);
+router.get('/', (req, res, next) => {
+  res.send('list of users');
 });
 
-router.post('/', (req,res,next)=>{
+router.get('/:param', (req, res, next) => {
+  res.send('get user with id: ' + req.params.param);
+});
 
-})
+router.post('/:param', (req, res, next) => {
+  res.send('Insert user with Id: ' + req.params.param);
+});
 
+router.put('/:param', (req, res, next) => {
+  res.send('Update User with Id: ' + req.params.param);
+});
+
+router.delete('/:param', (req, res, next) => {
+  res.send('deleted user with id: ' + req.params.param);
+});
 
 module.exports = router;
